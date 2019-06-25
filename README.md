509a10751d2620a4dcc476a62165b4a79454b432
EtienneLaval <etienne.m.laval@gmail.com>
2019-06-25 21:36:16 +0200

    clean but no unit tests

--- README.md
+++ README.md
@@ -1,20 +1,33 @@
-Démarche : 
-TODO enever le param element qi refere a page gerer la date
+## Lancement :
+initialisation et chargement des dépendances : 
 
+`$> npm install`
 
-Ma démarche est la suivante : j'ai  niveaux d'abstraction possibles : 
+pour lancer :  
+
+`$> npm start`
+
+pour lancer les tests:  
+
+`$> npm start`
+
+## Démarche : 
+
+Ma démarche est la suivante : Jai besoin de parser la data du html. Pour celà j'ai 3 niveaux d'abstraction possibles : 
 - la regexp dans le texte en tant que tel: une solution très simple mais sans doute assez fragile et qui plus est, les regexp sont peu user firndly et n'exploitent pas la structure du html. 
-- un parser du htlm en objet js : permet de coupler la simplicité et la main mise sur la solution tout en exloitant la structure du html, mais trouver une bonne mnière de parser peut être hardu
-- laisser un browser créer un DOM pour faciliter l'exploitaton de la donnée : de loin la solution la plus user fiendly mais sans doute aussi la plus gourmande en ressources.
+- un parser custom du htlm en objet js : permet de coupler la simplicité et la main mise sur la solution tout en exloitant la structure du html, mais trouver une bonne manière de parser peut être hardu
+- laisser un browser créer un DOM pour faciliter l'exploitaton de la donnée : de loin la solution la plus user fiendly mais sans doute aussi la plus gourmande en ressources. Plus tard j'ai réalisé que certains modules permettent de créer un dom et d'utiliser le xpath pour y trouver des éléments, mais les quelques tests effectués ne m'on pas permis de trouver des modules parfaitement fonctionnels sur les fonctionalités demandées. Ceci étant, c'est là clairement la voie à suivre pour alléger le projet. 
 
-J'ai décidé de partir sur cette dernière solution et d'utiliser puppeteer qui tourne sur un nuvigateur headless pour gagner en performance.
+J'ai décidé de partir sur cette dernière solution et d'utiliser puppeteer qui tourne sur un navigateur headless pour gagner en performance.
 
-Plus tard j'ai réalisé que certains modules permettent de créer un dom et d'utiliser le xpath pour y trouver des éléments, mais les quelques tests effectués ne m'on pas permis de trouver des modules parfaitement fonctionnels sur les fonctionalités demandées. Ceci étant, c'est là clairement la voie à suivre pour alléger le projet. 
+En suite j'ai essayé de créer un système d'instruction (cf instruction.json) se voulant aussi exhaustif que possible pour donner une solution plus élégante et plus facile à modifier qu'un script. Mis à part les dates des tickets de train (des structures répétées à l'interieur de chaines de carractère, alors que pupeteer ne supporte pas toutes les fonctions de string de xpath), j'estime que c'est relativement robuste.
 
-Nota bene :
+
+
+## Nota bene :
 Il me semble qu'il y a un souci dans le json demandé, en effet, lors du dernier retour, 4 passagés de 26 à 59 ans sont référencés alors que dans le html, seuls 2 sont de cette tranche d'age lorsque les 2 autres sont de jeunes enfants. J'ai donc ris la liberté de modifier le fichier attendu.
 
-Consignes
+## Consignes
 
 Le fichier test.html correspondant à des billets de train, et le fichier test-result.json qui est le résultat attendu. L'objectif est de réaliser un petit programme permettant de générer test-result.json.
 - langage libre
--- browser.js
+++ browser.js
@@ -6,24 +6,16 @@ const browseInstruction = async (instruction, element, responseObject, responseK
     const res = {}
     let endSubRepeat = false
     if ("__repeat" in instruction) {
-        if (!repeat) { repeat = [] }
-        delete instruction.__repeat
-        repeat[repeat.length] = 1
-        while (repeat.length > 0) {
-            await browseInstruction(instruction, element, responseObject, responseKey, repeat)
-        }
+        await handleRepeatingValue(instruction, element, responseObject, responseKey, repeat)
     } else {
         for (const key in instruction) {
             if (key == "__condition") {
-                let { __rule, __name, __content } = instruction[key]
-                if (__rule[0] == "repeat" && JSON.stringify(repeat) == __rule[1]) {
-                    if (Array.isArray(__content)) {
-                        res[__name] = []
-                        __content = __content[0]
-                    }
-                    await browseInstruction(__content, element, res[__name], __name, repeat ? Array.from(repeat) : undefined)
-                }
-            } else if ("xpath" in instruction[key]) {
+                await handleConditionValue(instruction, element, res, repeat)
+            } else if (Array.isArray(instruction[key])) {
+                if (!res[key]) { res[key] = [] }
+                await browseInstruction(instruction[key][0], element, res[key], 0, repeat ? Array.from(repeat) : undefined)
+            }
+            else if ("xpath" in instruction[key]) {
                 const searchedSubElement = await searchWithInstruction(instruction[key], element, repeat)
                 if (!searchedSubElement.length > 0) {
                     endSubRepeat = true
@@ -31,26 +23,21 @@ const browseInstruction = async (instruction, element, responseObject, responseK
                     res[key] = await treatSearchedElement(searchedSubElement, instruction[key], repeat)
                 }
             } else {
-                if (Array.isArray(instruction[key])) {
-                    if (!res[key]) { res[key] = [] }
-                    await browseInstruction(instruction[key][0], element, res[key], 0, repeat ? Array.from(repeat) : undefined)
-                }
-                else {
-                    await browseInstruction(instruction[key], element, res, key, repeat)
-                }
+                await browseInstruction(instruction[key], element, res, key, repeat)
             }
+
         }
         if (repeat) {
-            const length = repeat.length
             if (endSubRepeat) {
                 repeat.pop()
             } else {
                 repeat[repeat.length - 1]++
-                Array.isArray(responseObject) ? responseObject.push(res) : responseObject[responseKey] = res
             }
-        } else {
+        }
+        if (!endSubRepeat) {
             Array.isArray(responseObject) ? responseObject.push(res) : responseObject[responseKey] = res
         }
+
     }
 
 
@@ -75,6 +62,24 @@ const searchWhenMultipleTarget = async (subInstruction, element, repeat) => {
     return res
 
 }
+const handleConditionValue = async (instruction, element, res, repeat) => {
+    let { __rule, __name, __content } = instruction["__condition"]
+    if (JSON.stringify(eval(__rule[0])) == __rule[1]) {
+        if (Array.isArray(__content)) {
+            res[__name] = []
+            __content = __content[0]
+        }
+        await browseInstruction(__content, element, res[__name], __name, repeat ? Array.from(repeat) : undefined)
+    }
+}
+const handleRepeatingValue = async (instruction, element, responseObject, responseKey, repeat) => {
+    if (!repeat) { repeat = [] }
+    delete instruction.__repeat
+    repeat[repeat.length] = 1
+    while (repeat.length > 0) {
+        await browseInstruction(instruction, element, responseObject, responseKey, repeat)
+    }
+}
 const treatSearchedElement = async (searchedElement, subInstruction, repeat) => {
     let searchedText = await (await searchedElement[0].getProperty('textContent')).jsonValue()
     searchedText = await textParser.handleTreatment(subInstruction, searchedText, repeat)
@@ -86,4 +91,4 @@ const treatSearchedElement = async (searchedElement, subInstruction, repeat) => 
     return await searchedText
 }
 
-module.exports = {browseInstruction}
+module.exports = { browseInstruction }
